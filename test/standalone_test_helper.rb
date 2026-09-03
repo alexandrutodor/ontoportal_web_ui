498df127cedc658d5a05afe11dbda30b856d81fa
alexandrutodor <alexandru@todor.net>
2026-09-03 16:43:00 +0200

    fix(test): use explicit gem paths instead of recursive glob in test helper

--- test/standalone_test_helper.rb
+++ test/standalone_test_helper.rb
@@ -6,9 +6,12 @@ $LOAD_PATH.unshift File.expand_path('../app/models', __dir__)
 $LOAD_PATH.unshift File.expand_path('../app/services', __dir__)
 $LOAD_PATH.unshift File.expand_path('../app/controllers', __dir__)
 
-# Fallback path for rubyzip in test environment
-gem_paths = Dir.glob('/home/ranma/ontoportal/**/rubyzip-*/lib') + Dir.glob('/home/ranma/**/rubyzip-*/lib')
-gem_paths.uniq.each { |p| $LOAD_PATH.unshift(p) }
+# Explicit paths for rubyzip in test environment (avoid slow filesystem recursive globbing)
+["/home/ranma/ontoportal/ontoportal_web_ui/vendor/bundle/ruby/#{RUBY_VERSION}/gems/rubyzip-2.4.1/lib",
+ "/home/ranma/ontoportal/ontoportal_web_ui/vendor/bundle/ruby/3.3.0/gems/rubyzip-2.4.1/lib",
+ "/home/ranma/ontoportal/ontoportal_web_ui/vendor/bundle/ruby/3.1.0/gems/rubyzip-2.4.1/lib"].each do |p|
+  $LOAD_PATH.unshift(p) if File.directory?(p)
+end
 
 require 'semantic_capsule'
 require 'capsules/lockfile_generator'
